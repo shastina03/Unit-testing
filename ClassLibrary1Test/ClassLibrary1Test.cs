bcd82522db420f7c635ced3d71149de4b9b36271
shastina03 <73755402+shastina03@users.noreply.github.com>
2021-10-30 15:54:41 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1Test/ClassLibrary1Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace ClassLibrary1Test
+{
+    [TestClass]
+    public class ClassLibrary1Test
+    {
+        [TestMethod]
+        public void RactangleArea_3and_5_15returned()
+        {
+            //arrange
+            int a = 3, b = 5;
+            int expected = 15;
+
+            //act
+            Geometry g = new Geometry();
+            int actual =g.RectangleArea(a, b);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ ClassLibrary1Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClassLibrary1Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibrary1Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a2f72c5c-0f4d-41c0-ae04-a2ed98292c66")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
